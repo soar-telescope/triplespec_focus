3385624865c257ee9508af55a1233fff50410d0c
Simon Torres <simon.torres@noirlab.edu>
2023-08-03 16:04:51 -0400

    updated docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'test_', 'tests']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
 html_logo = '_static/soar_logo.png'
 html_context = {'license': 'BSD 3-Clause License'}
 
